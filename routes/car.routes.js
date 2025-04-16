18fab1a4ce85f118275d25662425ece5e2227a3b
GRIGORSARUKHANYAN <saruxanyan.grigor@gmail.com>
2025-04-16 16:15:35 +0400

    sdd

--- routes/car.routes.js
+++ routes/car.routes.js
@@ -8,8 +8,8 @@ const carController = new CarController();
 const upload = multer({ storage: multer.memoryStorage() });
 
 
-router.get("/:id",authMiddleware, carController.getCarById);
 router.get("carByUserId/:id",authMiddleware, carController.getCarByUserId);
+router.get("/:id",authMiddleware, carController.getCarById);
 
 router.post(
   "/",
